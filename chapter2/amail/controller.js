bcfd1d68722bb6f4589453e1b3ef7d21e081d374
johnnyzhang <1113638674@qq.com>
2016-08-23 00:38:21 +0800

    case of chapter2

--- chapter2/amail/controller.js
+++ chapter2/amail/controller.js
@@ -5,7 +5,7 @@
 // 为核心的AMail服务创建模块
 var aMailServices = angular.module('AMail',[]);
 // 在URL、模板和控制器之间建立映射关系
-function emailRouterConfig($routeProvider){
+aMailServices.config(function($routeProvider){
     $routeProvider
         .when('/',{
             controller: ListController,
@@ -19,13 +19,9 @@ function emailRouterConfig($routeProvider){
             redirectTo: '/'
         });
         // 注意，为了创建详情视图，我们在id前面加了一个冒号，从而指定一个参数化的URL组件
-
-}
-// 配置我们的路由，以便AMail服务能够找到它
-aMailServices.config(emailRouterConfig);
-
+});
 // 一些虚拟的邮件
-messages = [
+var messages = [
     {
         id: 0,
         sender: 'jean@somecompany.com',
@@ -52,22 +48,24 @@ messages = [
     }
 ];
 // 把我们的邮件发布给邮件的列表模板
-aMailServices.controller('ListController',
-    function($scope) {
-        $scope.messages =messages;
-    });
-// 从路由信息（从URL中解析出来的）中获取邮件的id,然后用它找到正确的邮件对象
-aMailServices.controller('DetailController',function ($scope,$routeParams) {
-   $scope.message = messages[$routeParams.id];
-});
+// aMailServices.controller('ListController',
+//     function($scope) {
+//         $scope.$route = $route;
+//         $scope.messages = messages;
+//     });
+// // 从路由信息（从URL中解析出来的）中获取邮件的id,然后用它找到正确的邮件对象
+// aMailServices.controller('DetailController',function ($scope,$routeParams) {
+//    $scope.message = messages[$routeParams.id];
+//     $scope.$route = $route;
+// });
 
 // Publish our messages for the list template
-// function ListController($scope) {
-//     $scope.messages = messages;
-// }
-//
-// // Get the message id from the route (parsed from the URL) and use it to
-// // find the right message object.
-// function DetailController($scope, $routeParams) {
-//     $scope.message = messages[$routeParams.id];
-// }
+function ListController($scope) {
+    $scope.messages = messages;
+}
+
+// Get the message id from the route (parsed from the URL) and use it to
+// find the right message object.
+function DetailController($scope, $routeParams) {
+    $scope.message = messages[$routeParams.id];
+}
