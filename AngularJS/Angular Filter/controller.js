6841b55a5df3c2a1a4a41d03771039711dfdcde6
johnnyzhang <1113638674@qq.com>
2016-10-01 22:22:52 +0800

    angular filter 练习

--- AngularJS/Angular Filter/controller.js
+++ AngularJS/Angular Filter/controller.js
@@ -1,12 +1,43 @@
 var app = angular.module('myApp', []);
 
-app.controller('PhoneListCtrl', function($scope) {
-    $scope.phones = [
-        {"name": "Nexus S",
-            "snippet": "Fast just got faster with Nexus S."},
-        {"name": "Motorola XOOM™ with Wi-Fi",
-            "snippet": "The Next, Next Generation tablet."},
-        {"name": "MOTOROLA XOOM™",
-            "snippet": "The Next, Next Generation tablet."}
-    ];
+app.controller('ListCtrl', function($scope,$http) {
+    $http.get('data.json').success(function(data) {
+        $scope.poi_data = data;
+        console.info('success');
+    });
+    $scope.orderProp = 'age';
+});
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/list.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        .when('/list', {
+            templateUrl: 'views/list.html',
+            controller: 'ListCtrl',
+            controllerAs: 'list'
+        })
+        .when('/university/:id',{
+            templateUrl: 'views/detail.html',
+            controller: 'DetailCtrl'
+        })
+        .when('/spot/:id',{
+            templateUrl: 'views/detail.html',
+            controller: 'DetailCtrl'
+        })
+        .when('/add', {
+            templateUrl: 'views/add.html',
+            controller: 'AddCtrl',
+            controllerAs: 'edit'
+        })
+        .when('/others', {
+            templateUrl: 'views/others.html',
+            controller: 'OthersCtrl',
+            controllerAs: 'others'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 });
